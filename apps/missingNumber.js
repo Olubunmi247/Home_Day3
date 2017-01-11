636ea31b1f6a5c58f03eddea30dcd383f3c813fd
Olubunmi247 <bhoomsie234@gmail.com>
2017-01-11 20:39:19 +0100

    finish work on missingNumber

---
+++ apps/missingNumber.js
@@ -0,0 +1,21 @@
+'use strict'
+module.exports = {
+
+findMissing: function(arr1, arr2){
+	var arr3 = [];
+	if((arr1.length === arr2.length) || arr1.length === arr2.length === 0){
+		return 0;
+	}
+	else{
+		arr1.forEach(function(val){
+			if(arr2.indexOf(val) < 0) arr3.push(val);
+		});
+		arr2.forEach(function(val){
+			if(arr1.indexOf(val) < 0) arr3.push(val);
+		});
+		var diff = arr3.pop();
+		return diff;
+	}
+}
+}
+ 
